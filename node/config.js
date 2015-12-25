55b4d45d968857714096e60ed3d62c66b1d7c9dd
Erik-jan Riemers <riemers@binkey.nl>
2015-12-25 01:44:31 +0100

    Update config.js

--- node/config.js
+++ node/config.js
@@ -2,10 +2,10 @@ module.exports = {
 	SteamAccounts: [],	// add 64-bit Steam Account ID's (as String) of users that should receive reports
 	SteamGroup: null,	// SteamGroup to send message to
 	logOnOptions: {		// add account credentials; see README.md
-		accountName: '',
+		account_name: '',
 		password: '',
-		authCode: '',
-		sentry: 'sentry'
+		auth_code: '', // only used if steam guard key is required. After that sentry file is created.
+		sha_sentryfile: 'sentry'
 	},
 	listenPort: 9876,	// port to listen on, must be the same as in the SourceMod plugin
 	reportPassword: '',	// required password, must be the same as in the SourceMod plugin
